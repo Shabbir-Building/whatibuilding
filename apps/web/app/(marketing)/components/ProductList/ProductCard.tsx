d5a10c52a791cb2ec477c14553f276198ca6d98f
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2026-01-10 00:37:30 +0600

    feat: productlist conts

--- apps/web/app/(marketing)/components/ProductList/ProductCard.tsx
+++ apps/web/app/(marketing)/components/ProductList/ProductCard.tsx
@@ -1,47 +1,57 @@
 import Link from "next/link";
-import IncomeChart from "./IncomeChart";
-import Image from "next/image";
-import ProductImage from "@/public/profile-image.png";
-import { CircleDollarSign, LockKeyhole } from "lucide-react";
 
-const ProductCard = () => {
+import Image from "next/image";
+
+import type { ProductCardProps } from "./types";
+
+import { CircleDollarSign, LockKeyhole } from "lucide-react";
+import { EARNINGS_STATUS } from "./constants";
+import { IncomeChart } from "./IncomeChart";
+
+const ProductCard = ({ product }: ProductCardProps) => {
   return (
     <div className="bg-white w-full max-w-120 rounded-2xl pb-4 overflow-visible">
       <Link
-        href="https://www.chattrn.com/"
+        href={product.productLink}
         target="_blank"
-        className="block p-6 w-full hover:rounded-2xl hover:scale-105 hover:bg-neutral-300 transition"
+        className="block p-5 w-full rounded-2xl hover:scale-103 hover:bg-neutral-300 transition"
       >
         <div className="flex justify-between items-center">
           <div className="flex items-center gap-2">
             <Image
               alt="product-image"
-              src={ProductImage}
+              src={product.imageSrc}
               width={24}
               height={24}
             />
-            <h2 className="text-md font-semibold">Chattrn</h2>
+            <h2 className="text-md font-semibold">{product.name}</h2>
           </div>
 
-          <span className="text-sm">
-            <span className="font-semibold">30</span> Active Users
+          <span className="text-xs">
+            <span className="font-semibold">{product.activeUsers}</span> Active
+            Users
           </span>
         </div>
-        <p className="text-sm text-neutral-600 mt-2">
-          Let the AI agent handle your customer
-        </p>
+        <p className="text-sm text-neutral-600 mt-2">{product.description}</p>
       </Link>
-      <IncomeChart />
-      <div className="px-3">
-        <div className="h-24 bg-gray-100 text-yellow-500 rounded-xl mt-2 p-4 text-xs w-full flex justify-center items-center">
-          <LockKeyhole size={14} className="mr-1" /> Earnings Confidential
+      {(product.earningsStatus === EARNINGS_STATUS.EARNINGS ||
+        product.earningsStatus === EARNINGS_STATUS.NO_EARNINGS) && (
+        <IncomeChart earningsData={product.earningsData ?? []} />
+      )}
+      {product.earningsStatus === EARNINGS_STATUS.CONFIDNETIALS && (
+        <div className="px-3">
+          <div className="bg-gray-100 h-24 text-yellow-500 rounded-xl p-4 text-xs w-full flex justify-center items-center">
+            <LockKeyhole size={14} className="mr-1" /> Earnings Confidential
+          </div>
         </div>
-      </div>
-      <div className="px-3">
-        <div className="h-24 bg-gray-100 text-yellow-500 rounded-xl mt-2 p-4 text-xs w-full flex justify-center items-center">
-          <CircleDollarSign size={14} className="mr-1" /> No Earnings Yet
+      )}
+      {/* {product.earningsStatus === EARNINGS_STATUS.NO_EARNINGS && (
+        <div className="px-3">
+          <div className="bg-gray-100 text-yellow-500 h-24 rounded-xl p-4 text-xs w-full flex justify-center items-center">
+            <CircleDollarSign size={14} className="mr-1" /> No Earnings Yet
+          </div>
         </div>
-      </div>
+      )} */}
     </div>
   );
 };
--- apps/web/app/(marketing)/components/ProductList/constants.ts
+++ apps/web/app/(marketing)/components/ProductList/constants.ts
@@ -48,6 +48,11 @@ export const monthlyIncomeData = Array.from(
 =======
 >>>>>>> feat: product card design complete
 
+export const monthlyZeroIncomeData = Array.from({ length: 12 }, (_, index) => ({
+  month: monthNames[index],
+  income: 0,
+}));
+
 // Chart configuration
 export const chartConfig = {
   income: {
@@ -55,3 +60,38 @@ export const chartConfig = {
     color: "hsla(41, 82%, 52%, 1.00)",
   },
 } satisfies ChartConfig;
+
+export const EARNINGS_STATUS = {
+  EARNINGS: 1,
+  NO_EARNINGS: 2,
+  CONFIDNETIALS: 3,
+};
+
+export const productList = [
+  {
+    name: "Chattrn",
+    productLink: "https://www.chattrn.com/",
+    imageSrc: "/chattrn.png",
+    description: "Let the AI agent handle your customer",
+    activeUsers: 7,
+    earningsStatus: EARNINGS_STATUS.CONFIDNETIALS,
+  },
+  {
+    name: "Talk Expense",
+    productLink: "https://www.talkexpense.com/",
+    imageSrc: "/talk-expense.png",
+    description: "Track your expenses easy with AI voice assistant",
+    activeUsers: 1,
+    earningsStatus: EARNINGS_STATUS.NO_EARNINGS,
+    earningsData: monthlyZeroIncomeData,
+  },
+  // {
+  //   name: "SalesBot",
+  //   imageSrc: "/profile-image.png",
+  //   description: "Track expenses easy with AI",
+  //   activeUsers: 20,
+  //   earningsStatus: EARNINGS_STATUS.EARNINGS,
+  //   earningsData: monthlyIncomeData,
+  //   productLink: "https://www.chattrn.com/",
+  // },
+];
