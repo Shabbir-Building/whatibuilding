1d291078ef0fe9694e492a2d7082a637cf093675
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2026-01-10 00:45:15 +0600

    fix: conflicts

--- apps/web/app/(marketing)/components/ProductList/constants.ts
+++ apps/web/app/(marketing)/components/ProductList/constants.ts
@@ -1,14 +1,5 @@
 import { ChartConfig } from "@/components/ui/chart";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-// Get current month (0-11)
-const currentMonth = new Date().getMonth();
-
->>>>>>> draft save for product list
-=======
->>>>>>> feat: product card design complete
 // Month names
 const monthNames = [
   "Jan",
@@ -25,32 +16,10 @@ const monthNames = [
   "Dec",
 ];
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> feat: product card design complete
 // Generate monthly income data for all 12 months
 export const monthlyIncomeData = Array.from({ length: 12 }, (_, index) => ({
   month: monthNames[index],
   income: Math.floor(Math.random() * 5000) + 2000, // Random income between $2000-$7000
-}));
-<<<<<<< HEAD
-=======
-// Generate monthly income data from January to current month
-export const monthlyIncomeData = Array.from(
-  { length: currentMonth + 1 },
-  (_, index) => ({
-    month: monthNames[index],
-    income: Math.floor(Math.random() * 5000) + 2000, // Random income between $2000-$7000
-  })
-);
->>>>>>> draft save for product list
-=======
->>>>>>> feat: product card design complete
-
-export const monthlyZeroIncomeData = Array.from({ length: 12 }, (_, index) => ({
-  month: monthNames[index],
-  income: 0,
 }));
 
 // Chart configuration
@@ -60,38 +29,3 @@ export const chartConfig = {
     color: "hsla(41, 82%, 52%, 1.00)",
   },
 } satisfies ChartConfig;
-
-export const EARNINGS_STATUS = {
-  EARNINGS: 1,
-  NO_EARNINGS: 2,
-  CONFIDNETIALS: 3,
-};
-
-export const productList = [
-  {
-    name: "Chattrn",
-    productLink: "https://www.chattrn.com/",
-    imageSrc: "/chattrn.png",
-    description: "Let the AI agent handle your customer",
-    activeUsers: 7,
-    earningsStatus: EARNINGS_STATUS.CONFIDNETIALS,
-  },
-  {
-    name: "Talk Expense",
-    productLink: "https://www.talkexpense.com/",
-    imageSrc: "/talk-expense.png",
-    description: "Track your expenses easy with AI voice assistant",
-    activeUsers: 1,
-    earningsStatus: EARNINGS_STATUS.NO_EARNINGS,
-    earningsData: monthlyZeroIncomeData,
-  },
-  // {
-  //   name: "SalesBot",
-  //   imageSrc: "/profile-image.png",
-  //   description: "Track expenses easy with AI",
-  //   activeUsers: 20,
-  //   earningsStatus: EARNINGS_STATUS.EARNINGS,
-  //   earningsData: monthlyIncomeData,
-  //   productLink: "https://www.chattrn.com/",
-  // },
-];
