1f0b7f9e99781249fc29574e86d1c0124d4c7c55
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2026-01-24 15:19:57 +0600

    add google analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 
 import "./globals.css";
 import { rubik } from "./fonts";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "what i building",
@@ -18,6 +19,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-B6NHK4DN9F"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-B6NHK4DN9F');
+          `}
+        </Script>
+      </head>
       <body className={`${rubik.className} antialiased`}>
         <div className="bg-stone-200 min-h-screen">{children}</div>
       </body>
